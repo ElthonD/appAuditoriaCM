ec71c6065da50338d4f1d17a52689862a6bfa9f4
ElthonD <elthon.rivas@gmail.com>
2023-11-06 11:44:38 -0600

    nuevo cambio

--- inicio.py
+++ inicio.py
@@ -16,7 +16,7 @@ def createPage():
     col2.image(display, use_column_width=True)
     #col2.title("Aplicación DataDriven")
 
-    col2.markdown('Bienvenido al panel de auditorías, está aplicación provee visualización de gráficos, indicadores y evidencias de las auditorías que se aplican a los procesos de Centro de Monitoreo y Línea de Reacción de AI27.')
+    col2.markdown('Bienvenido al panel de auditorías, está aplicación provee visualización de gráficos, indicadores y evidencias de las auditorías que se aplican a los procesos de monitoreo que aplican los agentes de monitoreo (Monitoristas y SOFIA) en el Centro de Monitoreo de AI27.')
 
     col2.write(""" 
     Está aplicación contiene:
